93f39d42847a4a4f7ddf67ad5488ad81e7922709
damonlim <damonlim@yahoo.com>
2021-11-12 09:54:59 +0800

    Changed the toast close again to 1502ms

--- src/components/Carlist.js
+++ src/components/Carlist.js
@@ -129,7 +129,7 @@ class Carlist extends Component {
         </Grid>
         <ReactTable data={this.state.cars} columns={columns} 
           filterable={true}/>
-        <ToastContainer autoClose={1501} /> 
+        <ToastContainer autoClose={1502} /> 
       </div>
     );
   }
